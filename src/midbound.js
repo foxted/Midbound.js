d89257ba6b7272c46e9eb5a2c7f8d99054821fe8
Valentin Prugnaud <valentin@whatdafox.com>
2016-07-18 04:57:30 -0700

    Starting tracker script. Load and execute queue.

--- src/midbound.js
+++ src/midbound.js
@@ -1,5 +1,38 @@
 (function () {
 
+    console.info('Midbound loading...');
+
+    // Fetch object name, timestamp and queue
+    var objectName = window['MidboundObject'] = window['MidboundObject'] || 'mb';
+    var timestamp = window[objectName].l;
+    var queue = window[objectName].q;
+
+    var Midbound = {
+        queue: queue,
+        timestamp: timestamp,
+        create: function() {
+            console.log('Create tracker');
+        },
+        send: function() {
+            console.log('Send information');
+        },
+        init: function() {
+            var i;
+            for (i = 0; i < this.queue.length; i++) {
+                this[queue[i][0]](queue[i]);
+            }
+            this.queue = [];
+        }
+    };
+
+    Midbound.init();
+
+    this[objectName] = function(name) {
+        if(typeof Midbound[name] === "function") {
+            Midbound[name].apply(null, arguments);
+        }
+    }
+
     console.info('Midbound loaded!');
 
 })();
