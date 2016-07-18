dde87676d63f524730354bdc57195d3e2ee89450
Valentin Prugnaud <valentin@whatdafox.com>
2016-07-18 00:07:20 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Midbound Tracker
---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var minify = require('gulp-minify');
+
+var paths = {
+    scripts: ['src/midbound.js']
+};
+
+gulp.task('scripts', function() {
+    gulp.src(paths.scripts)
+        .pipe(minify({
+            ext:{
+                min:'.js'
+            },
+            noSource: true
+        }))
+        .pipe(gulp.dest('build/'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(paths.scripts, ['scripts']);
+});
