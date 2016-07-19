c5e491c19788d6197458b1aec48ae11be247b9cd
Valentin Prugnaud <valentin@whatdafox.com>
2016-07-19 03:46:21 -0700

    Implement local storage, guid and start tracking pixel

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # Midbound Tracker
+
+Web tracker script to track visitors on client websites.
+
+## Universal Snippet
+
+This snippet will work on every browser that Midbound supports. (cf. Browser Support). 
+**This is the most recommended method.**
+
+```
+<script>
+    (function (m, i, d, w, a, y, s) {
+        m['MidboundObject'] = a;
+        m[a] = m[a] || function () { (m[a].q = m[a].q || []).push(arguments) }, m[a].l = 1 * new Date();
+        y = i.createElement(d), s = i.getElementsByTagName(d)[0];
+        y.async = 1;
+        y.src = w;
+        s.parentNode.insertBefore(y, s);
+    })(window, document, 'script', 'midbound.js', 'mb');
+
+    mb('create', 'UA-XXXXX-Y', 'auto');
+    mb('send', 'pageview');
+</script>
+```
+
+## Modern Browser Snippet
+
+This snippet will improve loading times on modern browsers, but will fall back 
+to a synchronous loading on older browsers that do not support the `async` attribute.
+
+```
+<script>
+    window.mb = window.mb || function(){ (mb.q = mb.q || []).push(arguments) }, mb.l =+ new Date;
+    mb('create', 'UA-XXXXX-Y', 'auto');
+    mb('send', 'pageview');
+</script>
+<script async src='../../build/midbound.js'></script>
+```
+
+## Browser Support
+
+| Chrome | Firefox | Safari | IE | Opera |
+|--------|---------|--------|----|-------|
+| 5 | 4.0 | 5 | 9 | 10.5 |
--- src/midbound.js
+++ src/midbound.js
@@ -2,24 +2,108 @@
 
     console.info('Midbound loading...');
 
-    // Fetch object name, timestamp and queue
+    /**
+     * Configuration
+     */
+    var localStorageKey = "midbound-guid";
+    var pixelUrl = "https://upload.wikimedia.org/wikipedia/commons/c/ce/Transparent.gif";
+
+    /**
+     * Fetch object name, timestamp and queue
+     */
     var objectName = window['MidboundObject'] = window['MidboundObject'] || 'mb';
     var timestamp = window[objectName].l;
     var queue = window[objectName].q;
 
+    /**
+     * Check user DoNotTrack Setting
+     */
+    var doNotTrack = window.navigator.doNotTrack || 0;
+    if(doNotTrack == true) {
+        console.info('Midbound disabled in respect for Universal Web Tracking Opt Out regulations: http://donottrack.us');
+        return false;
+    }
+
+    /**
+     * GUID Generator
+     * Creates GUID for user based on several different browser variables
+     * It will never be RFC4122 compliant but it is robust
+     * @returns {Number}
+     */
+    var guid = function() {
+        var nav = window.navigator;
+        var screen = window.screen;
+        var guid = nav.mimeTypes.length;
+
+        guid += nav.userAgent.replace(/\D+/g, '');
+        guid += nav.plugins.length;
+        guid += screen.height || '';
+        guid += screen.width || '';
+        guid += screen.pixelDepth || '';
+
+        return guid;
+    };
+
+    /**
+     * Create pixel
+     */
+    var createPixel = function() {
+        var img= new Image(1,1) || document.createElement('img');
+        img.src= pixelUrl;
+
+        return img;
+    }
+
+    /**
+     * Extract arguments
+     * @param arguments
+     * @returns {Array}
+     */
+    var extractArguments = function(arguments) {
+
+        var args = [];
+
+        if(Array.isArray(args)) {
+            args = Array.prototype.slice.call(arguments);
+        } else {
+            args = arguments;
+        }
+
+        args = args.splice(-1);
+
+        return args;
+    }
+
+    /**
+     * Look for previous Midbound ID
+     */
+    var midboundGuid = localStorage.getItem(localStorageKey);
+
+    /**
+     * Midbound object
+     * @type {{queue: *, timestamp: *, create: Midbound.create, send: Midbound.send, init: Midbound.init}}
+     */
     var Midbound = {
         queue: queue,
         timestamp: timestamp,
         create: function() {
-            console.log('Create tracker');
+            if(midboundGuid === null) {
+                localStorage.setItem(localStorageKey, guid());
+            }
         },
         send: function() {
             console.log('Send information');
+
+            var args = extractArguments(arguments);
+
+            // Create pixel with parameters
+
+            document.body.appendChild(createPixel());
         },
         init: function() {
             var i;
             for (i = 0; i < this.queue.length; i++) {
-                this[queue[i][0]](queue[i]);
+                this[queue[i][0]].apply(null, queue[i]);
             }
             this.queue = [];
         }
