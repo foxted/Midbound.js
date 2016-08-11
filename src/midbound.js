7890f154bbd830d7b8aee3c294b3fda33a4ef69a
Valentin Prugnaud <valentin@whatdafox.com>
2016-08-11 04:12:40 -0700

    Added form support

--- src/midbound.js
+++ src/midbound.js
@@ -4,7 +4,7 @@
      * Configuration
      */
     var localStorageGuidKey = "midbound-guid";
-    var pixelUrl = "https://app.midbound.io/_mb.gif";
+    var pixelUrl = "http://midbound.dev/_mb.gif";
 
     /**
      * Look for previous Midbound ID
@@ -18,10 +18,24 @@
     var queue = window[objectName].q;
 
     /**
+     * Form binding options
+     */
+    var midboundAttributeName = 'data-midbound-type';
+    var targetTypes = ['email', 'text'];
+    var nameLabels = [
+        'firstname', 'fname', 'first name', 'first-name',
+        'lastname', 'last name', 'lname', 'last-name',
+        'fullname', 'full name', 'name'
+    ];
+    var emailLabels = ['email', 'e-mail', 'e_mail', 'e mail'];
+    var companyLabels = ['company'];
+    var phoneLabels = ['phone'];
+
+    /**
      * Check user DoNotTrack Setting
      */
     var doNotTrack = window.navigator.doNotTrack || 0;
-    if(doNotTrack == true) {
+    if (doNotTrack == true) {
         console.info('Midbound disabled in respect for Universal Web Tracking Opt Out regulations: http://donottrack.us');
         return false;
     }
@@ -32,7 +46,7 @@
      * It will never be RFC4122 compliant but it is robust
      * @returns {Number}
      */
-    var guid = function() {
+    var guid = function () {
         var nav = window.navigator;
         var screen = window.screen;
         var guid = nav.mimeTypes.length;
@@ -49,15 +63,21 @@
     /**
      * Create pixel
      */
-    var createPixel = function(trackerId, guid, action, resource) {
-        var img= new Image(1,1) || document.createElement('img');
-
-        img.src= pixelUrl +
+    var createPixel = function (trackerId, guid, action, resource, type) {
+        var img = new Image(1, 1) || document.createElement('img');
+        var src = pixelUrl +
             '?midid=' + encodeURIComponent(trackerId) +
             '&midguid=' + encodeURIComponent(guid) +
             '&midac=' + encodeURIComponent(action) +
             '&midts=' + encodeURIComponent(Date.now()) +
-            '&midrc=' + encodeURIComponent(resource || window.location)
+            '&midrc=' + encodeURIComponent(resource || window.location);
+
+        // If capturing, set the type
+        if(type !== null) {
+            src += '&midtype=' + encodeURIComponent(type);
+        }
+
+        img.src = src;
     }
 
     /**
@@ -65,12 +85,12 @@
      * @param arguments
      * @returns {Array}
      */
-    var extractArguments = function(arguments, splice) {
+    var extractArguments = function (arguments, splice) {
 
         var args = [];
         splice = splice || 1;
 
-        if(Array.isArray(args)) {
+        if (Array.isArray(args)) {
             args = Array.prototype.slice.call(arguments);
         } else {
             args = arguments;
@@ -79,6 +99,110 @@
         args.splice(0, splice);
 
         return args;
+    }
+
+    /**
+     * Save the information on Midbound onblur
+     * @param inputValue
+     * @TODO Add some validation for emails
+     */
+    var saveInformationOnBlur = function (event) {
+        var type = event.target.getAttribute(midboundAttributeName);
+        var value = event.target.value || '';
+
+        if(value.trim().length > 0) {
+            mb('send', 'capture', value, type);
+        }
+    }
+
+    /**
+     * Bind midbound specific attributes
+     * @param field
+     * @param type
+     */
+    var bindMidboundAttributes = function(field, type) {
+        field.setAttribute(midboundAttributeName, type);
+    }
+
+    /**
+     * Validate a field and bind event on it if valid
+     * @param field
+     * @returns {boolean}
+     */
+    var validateAndBindField = function (field) {
+        var fieldType = field.getAttribute('type');
+        var fieldName = field.getAttribute('name');
+        var fieldClasses = field.getAttribute('class') || '';
+
+        if (targetTypes.indexOf(fieldType) < 0) {
+            return false;
+        }
+
+        var type = null;
+        var inputInformation = [];
+        var foundValidInformation = false;
+
+        inputInformation.push(fieldType);
+        inputInformation.push(fieldName);
+        inputInformation = inputInformation.concat(fieldClasses.split(' '));
+
+        inputInformation = inputInformation.filter(function(item) {
+            return item.length > 0;
+        });
+
+        for (var i = 0; i < inputInformation.length; i++) {
+            if (nameLabels.indexOf(inputInformation[i]) > -1) {
+                type = 'name';
+                foundValidInformation = true;
+            }
+            if (emailLabels.indexOf(inputInformation[i]) > -1) {
+                type = 'email';
+                foundValidInformation = true;
+            }
+            if (companyLabels.indexOf(inputInformation[i]) > -1) {
+                type = 'company';
+                foundValidInformation = true;
+            }
+            if (phoneLabels.indexOf(inputInformation[i]) > -1) {
+                type = 'phone';
+                foundValidInformation = true;
+            }
+
+            if(type !== null) {
+                bindMidboundAttributes(field, type);
+            }
+        }
+
+        return foundValidInformation;
+    };
+
+    /**
+     * Bind every valid field with Midbound listener
+     */
+    var bindingEventsOnFields = function () {
+        // Bind listener on every input with specific criteria
+        var inputs = document.getElementsByTagName('input');
+        for (var i = 0; i < inputs.length; i++) {
+            if (validateAndBindField(inputs[i])) {
+                inputs[i].addEventListener("blur", saveInformationOnBlur);
+            }
+        }
+    }
+
+    /**
+     * Add midbound guid hidden field in form
+     * @param guid
+     */
+    var addMidboundGuidToForms = function (guid) {
+        var forms = document.forms;
+
+        for (var i = 0; i < forms.length; i++) {
+            var input = document.createElement("input");
+            input.type = "hidden";
+            input.name = "midboundGuid";
+            input.value = guid;
+            forms[i].appendChild(input);
+        }
     }
 
     /**
@@ -93,11 +217,17 @@
          * Create tracker
          * @param midboundId
          */
-        create: function(midboundId) {
+        create: function (midboundId, mode) {
             this.midboundId = midboundId;
+            this.mode = mode || 'auto';
 
-            if(midboundGuid === null) {
+            if (midboundGuid === null) {
                 localStorage.setItem(localStorageGuidKey, guid());
+            }
+
+            if (this.mode === 'auto') {
+                addMidboundGuidToForms(localStorage.getItem(localStorageGuidKey));
+                bindingEventsOnFields();
             }
         },
 
@@ -106,19 +236,20 @@
          * @param actionType
          * @param resource
          */
-        send: function(actionType, resource) {
+        send: function (actionType, resource, type) {
             createPixel(
                 this.midboundId,
                 localStorage.getItem(localStorageGuidKey),
                 actionType,
-                resource
+                resource,
+                type
             );
         },
 
         /**
          * Initialize event queue
          */
-        init: function() {
+        init: function () {
             var i;
             for (i = 0; i < this.queue.length; i++) {
                 this[queue[i][0]].apply(null, extractArguments(queue[i]));
@@ -131,8 +262,8 @@
     Midbound.init();
 
     // Replace queue with active tracker
-    this[objectName] = function(name) {
-        if(typeof Midbound[name] === "function") {
+    this[objectName] = function (name) {
+        if (typeof Midbound[name] === "function") {
             Midbound[name].apply(null, extractArguments(arguments));
         }
     }
